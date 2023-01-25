2b09986efec41b78baf25a7fc44192c5fe134ab5
cherbourg@fub.fr <cherbourg@fub.fr>
2023-01-25 22:19:45 +0100

    Update config.js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+/*
+|--------------------------------------------------------------------------
+| Server.js 
+|--------------------------------------------------------------------------
+|
+| Here is the server.js
+| 
+| 
+|
+*/
+
+// Load in our dependencies
+const express = require('express');
+const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const server = express();
+
+
+export default server; 
+
+
+
