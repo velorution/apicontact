b59924ac8d7b8c875c17c80e3438093607ccfb7e
Awazez <martin.aubeut@gmail.com>
2023-01-25 19:08:34 +0100

    rename index.js en server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+import server from './config/server';
+// A mettre dans le fichier .env 
+const PORT = 5000;
+
+// Launch our app on port 3000
+server.listen(PORT, () => console.log("Server is running!"));
