58bf48d65e34f9eaa6157a15dc0d0dadf809d7db
Martin AUBEUT <51866468+Awazez@users.noreply.github.com>
2023-02-03 19:49:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
-const { expressjwt: expressJwt } = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const sibApiV3Sdk = require('sendinblue-api');
-const {apiKey} = require('./config');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
 
-const server = express();
-server.use(express.json());
-
-const checkJwt = expressJwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-  }),
-
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256'],
+app.post('/send-email', async (req, res) => {
+  try {
+    const { to, subject, text } = req.body;
+    const response = await axios.post('https://api.sendinblue.com/v3/smtp/email', {
+      to: [{ email: to }],
+      subject,
+      text,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': 'YOUR_API_KEY',
+      },
+    });
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error sending email' });
+  }
 });
 
-server.use(checkJwt);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
