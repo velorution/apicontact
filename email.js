8c3a15bf82eb79fd3ad900fdf3a60d3134582913
Awazez <martin.aubeut@gmail.com>
2023-02-18 21:43:02 +0100

    Add connection to the api send in blue

--- email.js
+++ email.js
@@ -1,19 +1,36 @@
-const sendEmail = async (req, res) => {
-    const { to, subject, htmlContent } = req.body;
-    if (!req.user) {
-      return res.status(401).json({ message: 'Not authorized' });
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+require('dotenv').config()
+
+// Configure API key authorization: api-key
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.API_KEY;
+
+// Uncomment below two lines to configure authorization using: partner-key
+// var partnerKey = defaultClient.authentications['partner-key'];
+// partnerKey.apiKey = 'YOUR API KEY';
+
+var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
+sendSmtpEmail = {
+    to: [{
+        email: 'martin.aubeut@gmail.com',
+        name: 'John Doe'
+    }],
+    templateId: 59,
+    params: {
+        name: 'John',
+        surname: 'Doe'
+    },
+    headers: {
+        'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
     }
-    const sendSmtpEmail = new sibApiV3Sdk.SendSmtpEmail();
-    sendSmtpEmail.to = [{email: to}];
-    sendSmtpEmail.subject = subject;
-    sendSmtpEmail.htmlContent = htmlContent;
-  
-    try {
-      const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
-      res.status(200).json({ message: 'Email sent successfully', result });
-    } catch (error) {
-      res.status(400).json({ message: 'Error sending email', error });
-    }
-  }
-  
-  module.exports = { sendEmail };
+};
+
+apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+  console.log('API called successfully. Returned data: ' + data);
+}, function(error) {
+  console.error(error);
+});
